1bc9fd7beb51951aa180e9eb8c62371ff552d6d2
0xzoz <zeroexzoz@gmail.com>
2022-12-21 22:22:37 -0800

    Update webpage design

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
+import React, { useEffect, useRef, useState } from 'react';
 import video from './assets/video/trees-2.mp4';
 import logo from './assets/images/web3socialcapital.png';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import EmailIcon from '@mui/icons-material/Email';
 import './App.css';
 
+
+
 function App() {
+
+  const arrowRef = useRef(null);
+  const [originalTop, setOriginalTop] = useState(0);
+
+  useEffect(() => {
+    setOriginalTop(arrowRef.current.offsetTop);
+  }, []);
+
+  useEffect(() => {
+    let top = originalTop;
+    const interval = setInterval(() => {
+      console.log(top);
+      top += -5;
+      console.log(top);
+      if (top <= 30) {
+        top = originalTop;
+      }
+      arrowRef.current.style.top = top + 'px';
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, [originalTop]);
+
+
   return (
+    <>
     <div className="App">
+      
       <div className='App-header'>
-        <div className='box'>
-        <pre className='logo-text'>Web3 Social Capital</pre>
+        <div className='box-logo'>
+          <pre className='logo-text'>Web3 Social Capital</pre>
         </div>
-        <div className='box'>
-        {/* <img className='logo' src={logo}></img> */}
+
+        <div className='box-logo'>
+          <a href='mailto:zoz@web3socialcapital.xyz'><EmailIcon style={{ color: 'white' }}/></a>
+          <a href='https://twitter.com/W3S0C'><TwitterIcon style={{ color: 'white' }}/></a>
         </div>
       </div>
-      <video className='background-video' autoPlay loop muted>
-          <source src={video} type='video/mp4' />
-      </video>
 
+      <video className='background-video' autoPlay loop muted>
+          <source src="https://cdn.pixabay.com/vimeo/334228164/Loop%20-%2023167.mp4?width=1280&expiry=1671689679&hash=e45672268b64eed892ad02a2a72e6a4a655689b6" type='video/mp4' />
+      </video>
+      
       <div className="footer">
-        <a href='https://twitter.com'><EmailIcon style={{ color: 'white' }}/></a>
-        <a href='https://twitter.com'><TwitterIcon style={{ color: 'white' }}/></a>
+        <div ref={arrowRef} id="arrow">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" height="48" width="48">
+            <path d="M22.5 42V11.7L18 16.2l-2.1-2.1L24 6l8.1 8.1-2.1 2.1-4.5-4.5V42Z"/>
+          </svg>
+        </div>
       </div>
 
     </div>
+
+    <div className="page-2">
+    <div class="container">
+      <div class="box box1"><pre><a href='https://thepassivetrust.com/'>The Passive Trust</a> | <b>INFRACSTRUCTURE</b></pre></div>
+      <div class="box box2"><pre><a href='https://twitter.com/Explain_like_im'>@Explain_Like_Im</a> | <b>PRODUCT</b></pre></div>
+      <div class="box box3"><pre><a href='https://prezenti.xyz'>Prezenti</a> | <b>CURATION</b></pre></div>
+      <div class="box box4"></div>
+    </div>
+    </div>
+    </>
   );
 }
 
